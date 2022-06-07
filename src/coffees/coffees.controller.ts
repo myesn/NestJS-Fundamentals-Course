c3fbce98c27ed20aeeb3c0d949f546f0d6a6edcd
myesn <myesn@foxmail.com>
2022-06-07 11:37:13 +0800

    :sparkles: 添加动态路由和解析方式

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,9 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
 
 @Controller('coffees')
 export class CoffeesController {
   @Get()
   findAll() {
     return 'This action returns all coffees';
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string) {
+    return `This action returns #${id} coffee`;
   }
 }
