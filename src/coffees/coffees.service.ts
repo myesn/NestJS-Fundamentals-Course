c1b11041a9ae85e7a9bf9ba9d386cf9ac78e5d36
myesn <myesn@foxmail.com>
2022-06-09 16:04:15 +0800

    :sparkles: 创建 CoffeesService，并重构 CoffeesController 中的代码

---
+++ src/coffees/coffees.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@nestjs/common';
+import { Coffee } from './entities/coffee.entity';
+
+@Injectable()
+export class CoffeesService {
+  // 模拟数据源(datasource)，从中读取、更新和删除 item
+  private coffees: Coffee[] = [
+    {
+      id: 1,
+      name: 'Shipwreck Roast',
+      brand: 'Buddy Brew',
+      flavors: ['chocolate', 'vanilla'],
+    },
+  ];
+
+  findAll() {
+    return this.coffees;
+  }
+
+  findOne(id: string) {
+    return this.coffees.find((item) => item.id === +id);
+  }
+
+  create(body: any) {
+    this.coffees.push(body);
+  }
+
+  update(id: string, updateBody: any) {
+    const existingCoffee = this.findOne(id);
+    if (existingCoffee) {
+      // update the existing entity
+    }
+  }
+
+  remove(id: string) {
+    const coffeeIndex = this.coffees.findIndex((item) => item.id === +id);
+    if (coffeeIndex >= 0) {
+      this.coffees.splice(coffeeIndex, 1);
+    }
+  }
+}
---
+++ src/coffees/entities/coffee.entity.ts
@@ -0,0 +1,7 @@
+// Coffee 资源实体
+export class Coffee {
+  id: number;
+  name: string;
+  brand: string;
+  flavors: string[];
+}
