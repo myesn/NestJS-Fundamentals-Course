33dff12bd3aecd8edaf5f34693e97bb1f0969ffe
myesn <myesn@foxmail.com>
2022-06-13 15:07:35 +0800

    :art: 在 controller 和 service 中修改 create、update 函数中的参数命名，和视频中保持一致吧

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -28,13 +28,13 @@ export class CoffeesController {
   }
 
   @Post()
-  create(@Body() body: CreateCoffeeDto) {
-    return this.coffeesService.create(body);
+  create(@Body() createCoffeeDto: CreateCoffeeDto) {
+    return this.coffeesService.create(createCoffeeDto);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() body: UpdateCoffeeDto) {
-    return this.coffeesService.update(id, body);
+  update(@Param('id') id: string, @Body() updateCoffeeDto: UpdateCoffeeDto) {
+    return this.coffeesService.update(id, updateCoffeeDto);
   }
 
   @Delete(':id')
--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -26,12 +26,12 @@ export class CoffeesService {
     return coffee;
   }
 
-  create(body: any) {
-    this.coffees.push(body);
-    return body;
+  create(createCoffeeDto: any) {
+    this.coffees.push(createCoffeeDto);
+    return createCoffeeDto;
   }
 
-  update(id: string, updateBody: any) {
+  update(id: string, updateCoffeeDto: any) {
     const existingCoffee = this.findOne(id);
     if (existingCoffee) {
       // update the existing entity
