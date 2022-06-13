bd33f5c23a0fc7dae7168feeb8a3aff42c38e8ea
myesn <myesn@foxmail.com>
2022-06-13 14:31:39 +0800

    :sparkles: 为 createCoffeeDto 添加规则注解

--- src/coffees/dto/create-coffee.dto.ts
+++ src/coffees/dto/create-coffee.dto.ts
@@ -1,6 +1,14 @@
+import { IsString } from 'class-validator';
+
 // 创建 coffee 的数据模型
 export class CreateCoffeeDto {
+  @IsString()
   readonly name: string;
+
+  @IsString()
   readonly brand: string;
+
+  // each: true 表示期望值是一个字符串数组
+  @IsString({ each: true })
   readonly flavors: string[];
 }
