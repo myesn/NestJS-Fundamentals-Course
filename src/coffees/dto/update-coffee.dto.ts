0da32b1e038dd791d7b5c2bc1e14241948929892
myesn <myesn@foxmail.com>
2022-06-13 14:44:24 +0800

    :recycle: 消除 UpdateCoffeeDto 的字段冗余

--- src/coffees/dto/update-coffee.dto.ts
+++ src/coffees/dto/update-coffee.dto.ts
@@ -1,6 +1,5 @@
+import { PartialType } from '@nestjs/mapped-types';
+import { CreateCoffeeDto } from './create-coffee.dto';
+
 // 修改 coffee 的数据模型
-export class UpdateCoffeeDto {
-  readonly name?: string;
-  readonly brand?: string;
-  readonly flavors?: string[];
-}
+export class UpdateCoffeeDto extends PartialType(CreateCoffeeDto) {}
