f46467e529beadb6397494dd2ad3d66ed99f6bcc
myesn <myesn@foxmail.com>
2022-06-30 02:23:34 +0000

    ✨ 启用 Coffee 实体的级联功能

--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -19,6 +19,8 @@ export class Coffee {
   brand: string;
 
   @JoinTable() // 关系所有者端才需要加这个修饰符，即主表要加，外表不加
-  @ManyToMany((type) => Flavor, (flavor) => flavor.coffees)
+  @ManyToMany((type) => Flavor, (flavor) => flavor.coffees, {
+    cascade: true, // boolean | ("insert" | "update" | "remove" | "soft-remove" | "recover")[];
+  })
   flavors: string[];
 }
