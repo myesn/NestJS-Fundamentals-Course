c637805d6639d8cf38a5726589a2120c52306dcc
myesn <myesn@foxmail.com>
2022-06-30 07:05:16 +0000

    ✨ 添加 Event Entity

---
+++ src/events/entities/event.entity.ts
@@ -0,0 +1,16 @@
+import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class EventEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  type: string;
+
+  @Column()
+  name: string;
+
+  @Column('json')
+  payload: Record<string, any>; // Record 采用 Record<K, T> 的形式，其中 K 是键的类型，T 是值的类型。https://fjolt.com/article/typescript-record-type
+}
