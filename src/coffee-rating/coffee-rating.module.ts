cd709636ab661aa2227bf76a9d43c03ef2ac7056
myesn <myesn@foxmail.com>
2022-07-08 09:21:23 +0000

    ✨ 在 CoffeeRatingModule 中导入 CoffeesModule

--- src/coffee-rating/coffee-rating.module.ts
+++ src/coffee-rating/coffee-rating.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
+import { CoffeesModule } from 'src/coffees/coffees.module';
 import { CoffeeRatingService } from './coffee-rating.service';
 
 @Module({
-  providers: [CoffeeRatingService]
+  imports: [CoffeesModule],
+  providers: [CoffeeRatingService],
 })
 export class CoffeeRatingModule {}
