fbbd891d9e86331070ae4c67f8c549006c52f273
myesn <myesn@foxmail.com>
2022-07-08 07:42:53 +0000

    ✨ 增加 CoffeeRatingModule

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -9,6 +9,14 @@ import { Flavor } from './entities/flavor.entity';
 @Module({
   imports: [TypeOrmModule.forFeature([Coffee, Flavor, Event])],
   controllers: [CoffeesController],
+  /**
+   * providers: [
+   *  {
+   *    provide: CoffeesService,   // Injection token
+   *    useClass: CoffeesService,  // Type (class name) of provider (instance to be injected).
+   *  }
+   * ]
+   */
   providers: [CoffeesService],
 })
 export class CoffeesModule {}
