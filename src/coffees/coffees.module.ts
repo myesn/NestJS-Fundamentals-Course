a3c74723e96a908e5a0b10d42c9f348998861e55
myesn <myesn@foxmail.com>
2022-07-11 09:02:48 +0000

    ✨ 注入和解析 Nonclassbased Provider Token

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Event } from 'src/events/entities/event.entity';
+import { COFFEE_BRANDS } from './coffees.constants';
 import { CoffeesController } from './coffees.controller';
 import { CoffeesService } from './coffees.service';
 import { Coffee } from './entities/coffee.entity';
@@ -17,7 +18,13 @@ import { Flavor } from './entities/flavor.entity';
    *  }
    * ]
    */
-  providers: [CoffeesService], // 注入到 IOC 容器
+  providers: [
+    CoffeesService,
+    {
+      provide: COFFEE_BRANDS,
+      useValue: ['buddy brew', 'nescafe'],
+    },
+  ], // 注入到 IOC 容器
   exports: [CoffeesService], // 导出为本模块的公共 Provider，即可以被其他模块解析使用
 })
 export class CoffeesModule {}
--- src/coffees/coffees.service.ts
+++ src/coffees/coffees.service.ts
@@ -1,4 +1,4 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Inject, Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { PaginationQueryDto } from '../common/dto/pagination-query.dto';
 import { Connection, Repository } from 'typeorm';
@@ -7,6 +7,7 @@ import { UpdateCoffeeDto } from './dto/update-coffee.dto';
 import { Coffee } from './entities/coffee.entity';
 import { Flavor } from './entities/flavor.entity';
 import { Event } from 'src/events/entities/event.entity';
+import { COFFEE_BRANDS } from './coffees.constants';
 
 @Injectable()
 export class CoffeesService {
@@ -16,6 +17,7 @@ export class CoffeesService {
     @InjectRepository(Flavor)
     private readonly flavorRepository: Repository<Flavor>,
     private readonly connection: Connection,
+    @Inject(COFFEE_BRANDS) coffeeBrands: string[],
   ) {}
 
   findAll(paginationQuery: PaginationQueryDto) {
