3fe99ac1d0b2d5f4a2d1d2ff00b8a0b84c6c16cc
myesn <myesn@foxmail.com>
2022-07-12 02:58:05 +0000

    ✨ async/await 和 useFactory 和 inject 的结合使用

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,6 +1,7 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Event } from 'src/events/entities/event.entity';
+import { Connection } from 'typeorm';
 import { COFFEE_BRANDS } from './coffees.constants';
 import { CoffeesController } from './coffees.controller';
 import { CoffeesService } from './coffees.service';
@@ -22,7 +23,13 @@ import { Flavor } from './entities/flavor.entity';
     CoffeesService,
     {
       provide: COFFEE_BRANDS,
-      useValue: ['buddy brew', 'nescafe'],
+      useFactory: async (connection: Connection): Promise<string[]> => {
+        // const coffeeBrands = await connection.query('select * ...');
+        const coffeeBrands = await Promise.resolve(['buddy brew', 'nescafe']);
+        console.log('[!] Async factory');
+        return coffeeBrands;
+      },
+      inject: [Connection],
     },
   ], // 注入到 IOC 容器
   exports: [CoffeesService], // 导出为本模块的公共 Provider，即可以被其他模块解析使用
