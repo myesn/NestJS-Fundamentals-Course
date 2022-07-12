665dae522833f893fb399aa1089cd7f6e5dea257
myesn <myesn@foxmail.com>
2022-07-12 03:59:19 +0000

    ✨ Dynamic Module 示例（通用模块在不同的其他模块中表现不同）

--- src/coffee-rating/coffee-rating.module.ts
+++ src/coffee-rating/coffee-rating.module.ts
@@ -1,9 +1,19 @@
 import { Module } from '@nestjs/common';
 import { CoffeesModule } from 'src/coffees/coffees.module';
+import { DatabaseModule } from 'src/database/database.module';
 import { CoffeeRatingService } from './coffee-rating.service';
 
 @Module({
-  imports: [CoffeesModule],
+  imports: [
+    CoffeesModule,
+    DatabaseModule.register({
+      type: 'postgres',
+      host: 'localhost',
+      username: 'postgres',
+      password: 'pass123',
+      port: 5432,
+    }),
+  ],
   providers: [CoffeeRatingService],
 })
 export class CoffeeRatingModule {}
